50d7d39aa849c89386353a3e198cd1cc5c437e06
azzmnrwebdev <azzmnrwebdev@gmail.com>
2025-06-30 10:37:54 +0700

    ubah bulan pada education

--- src/utils/Educations.js
+++ src/utils/Educations.js
@@ -7,8 +7,8 @@ const educations = [
     name_en: "Nurul Fikri College of Integrated Technology",
     position_id: "S1 - Teknik Informatika",
     position_en: "Bachelor's Degree in Informatics Engineering",
-    date_id: "Agu 2019 - Agu 2023",
-    date_en: "Aug 2019 - Aug 2023",
+    date_id: "Sep 2019 - Sep 2023",
+    date_en: "Sep 2019 - Sep 2023",
   },
 ];
 
